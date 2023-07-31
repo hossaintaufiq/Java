e51c976339fcb9373091bbd8b85b246ca7efb69f
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2023-07-31 18:56:46 +0600

    restarting code

--- Introduction.java
+++ Introduction.java
@@ -54,13 +54,27 @@
 
 // }
 //  adding string 
+// import java.util.Scanner;
+// public class Introduction {
+//   public static void main(String[] args){
+//     System.out.println("Enter your name: ");*
+//     Scanner s=new Scanner(System.in);
+//     String name=s.nextLine();
+//     s.close();
+//     System.out.println("Your name is " + name);
+//   }
+// }
+
+
+// fully myself
+
 import java.util.Scanner;
-public class Introduction {
+public class Introduction{
   public static void main(String[] args){
-    System.out.println("Enter your name: ");
+    System.out.println("Enter your name in: ");
     Scanner s=new Scanner(System.in);
     String name=s.nextLine();
     s.close();
-    System.out.println("Your name is " + name);
-  }
+    System.out.println("Your name is : "+name);
+}
 }
