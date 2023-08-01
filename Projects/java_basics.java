cd55dcbd12cea22ebd945d5d12ee1fa135db4745
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2023-08-01 09:32:46 +0600

    java new

---
+++ Projects/java_basics.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class java_basics {
+    public static void main(String[] args){
+    //    addition of java
+
+    int a =20;
+    int b=15;
+
+    int multiply=a*b;
+
+    System.out.println(multiply);
+
+
+
+    }
+}
